9ba79189d60ec5b62bfc17afcc407481cc9c9957
Wasif Khan <waasif.khaan@gmail.com>
2018-11-11 19:20:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+function init() {
+  // Write your JavaScript code inside the init() function
+  let index = 0;
+
   const main = document.querySelector('body');
-function onKeyDownHandler(e) {
+// This is the function that would be invoked by the event listener.
+main.addEventListener('keydown', function(e) {
   const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -14,14 +20,7 @@ function onKeyDownHandler(e) {
   } else {
     index = 0;
   }
-}
+});
 
-function init() {
-  // Write your JavaScript code inside the init() function
-  let index = 0;
-
-
-// This is the function that would be invoked by the event listener.
-
-return main.addEventListener('keydown',onKeyDownHandler);
 };
+init();
